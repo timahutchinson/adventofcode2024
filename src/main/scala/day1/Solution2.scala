0e0e2e57512655bc44d6f84f075fbcc30b2032e2
Tim Hutchinson <tim.hutchinson@peopledatalabs.com>
2024-12-01 22:32:36 -0700

    tidy

--- src/main/scala/day1/Solution2.scala
+++ src/main/scala/day1/Solution2.scala
@@ -1,13 +1,16 @@
 package day1
 
 import scala.io.Source
+import scala.util.Using
 
 object Solution2 extends App {
   private def solution() = {
     val startTime = System.nanoTime()
 
     val filePath: String = "src/main/scala/day1/input.txt"
-    val lines = Source.fromFile(filePath).getLines().toList
+    val lines = Using.resource(Source.fromFile(filePath)) { source =>
+      source.getLines().toList
+    }
     val (col1, col2) = lines
       .map(_.trim.split("\\s+"))
       .filter(_.length == 2)
@@ -30,6 +33,6 @@ object Solution2 extends App {
   private val (solutions, times) = (1 to 100).map { _ => solution() }.unzip
 
   println(s"Solution: ${solutions.head}")
-  private val slicedTimes = times.drop(5)
+  private val slicedTimes = times.drop(5) // skipping JVM warmup
   println(f"Average runtime: ${slicedTimes.sum / slicedTimes.size}%.3f ms")
 }
