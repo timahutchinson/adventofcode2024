6e5ceb013f9e017386ec5729b52da91c63dc387e
Tim Hutchinson <tim.hutchinson@peopledatalabs.com>
2024-12-01 22:26:50 -0700

    added timing for fun

--- src/main/scala/day1/Solution1.scala
+++ src/main/scala/day1/Solution1.scala
@@ -3,23 +3,34 @@ package day1
 import scala.io.Source
 
 object Solution1 extends App {
-  val filePath: String = "src/main/scala/day1/input.txt"
+  private def solution() = {
+    val startTime = System.nanoTime()
 
-  val lines = Source.fromFile(filePath).getLines().toList
+    val filePath: String = "src/main/scala/day1/input.txt"
+    val lines = Source.fromFile(filePath).getLines().toList
+    val (col1, col2) = lines
+      .map(_.trim.split("\\s+"))
+      .filter(_.length == 2)
+      .map { case Array(a, b) => (a.toInt, b.toInt) }
+      .unzip
 
-  val (col1, col2) = lines
-    .map(_.trim.split("\\s+"))
-    .filter(_.length == 2)
-    .map { case Array(a, b) => (a.toInt, b.toInt) }
-    .unzip
+    val sortedCol1 = col1.sorted
+    val sortedCol2 = col2.sorted
 
-  val sortedCol1 = col1.sorted
-  val sortedCol2 = col2.sorted
+    val sumOfDifferences = sortedCol1
+      .zip(sortedCol2)
+      .map { case (a, b) => math.abs(a - b) }
+      .sum
 
-  val sumOfDifferences = sortedCol1
-    .zip(sortedCol2)
-    .map { case (a, b) => math.abs(a - b) }
-    .sum
+    val endTime = System.nanoTime()
+    val elapsedTime = (endTime - startTime) / 1e6
 
-  println(s"Solution: $sumOfDifferences")
+    (sumOfDifferences, elapsedTime)
+  }
+
+  private val (solutions, times) = (1 to 20).map { _ => solution() }.unzip
+
+  println(s"Solution: ${solutions.head}")
+  private val slicedTimes = times.drop(5)
+  println(f"Average runtime: ${slicedTimes.sum / slicedTimes.size}%.3f ms")
 }
--- src/main/scala/day1/Solution2.scala
+++ src/main/scala/day1/Solution2.scala
@@ -3,22 +3,33 @@ package day1
 import scala.io.Source
 
 object Solution2 extends App {
-  val filePath: String = "src/main/scala/day1/input.txt"
+  private def solution() = {
+    val startTime = System.nanoTime()
 
-  val lines = Source.fromFile(filePath).getLines().toList
+    val filePath: String = "src/main/scala/day1/input.txt"
+    val lines = Source.fromFile(filePath).getLines().toList
+    val (col1, col2) = lines
+      .map(_.trim.split("\\s+"))
+      .filter(_.length == 2)
+      .map { case Array(a, b) => (a.toInt, b.toInt) }
+      .unzip
 
-  val (col1, col2) = lines
-    .map(_.trim.split("\\s+"))
-    .filter(_.length == 2)
-    .map { case Array(a, b) => (a.toInt, b.toInt) }
-    .unzip
+    val col2FrequencyMap = col2.groupBy(identity).view.mapValues(_.size).toMap
+    val col1Frequencies = col1.map(value => col2FrequencyMap.getOrElse(value, 0))
+    val similarityScore = col1
+      .zip(col1Frequencies)
+      .map { case (a, b) => a * b }
+      .sum
 
-  val col2FrequencyMap = col2.groupBy(identity).view.mapValues(_.size).toMap
-  val col1Frenquencies = col1.map(value => col2FrequencyMap.getOrElse(value, 0))
-  val similarityScore = col1
-    .zip(col1Frenquencies)
-    .map { case (a, b) => a * b }
-    .sum
+    val endTime = System.nanoTime()
+    val elapsedTime = (endTime - startTime) / 1e6
 
-  println(s"Solution: $similarityScore")
+    (similarityScore, elapsedTime)
+  }
+
+  private val (solutions, times) = (1 to 100).map { _ => solution() }.unzip
+
+  println(s"Solution: ${solutions.head}")
+  private val slicedTimes = times.drop(5)
+  println(f"Average runtime: ${slicedTimes.sum / slicedTimes.size}%.3f ms")
 }
